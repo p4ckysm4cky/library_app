340dbd9d04adde40b2c2dd68bc04195ed326594a
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2021-07-24 13:30:13 +1200

    Added Book class and method to add rows

--- myscript.js
+++ myscript.js
@@ -1,16 +1,87 @@
-const title = document.getElementById("Title")
-const author = document.getElementById("Author")
-const status = document.getElementById("Status")
-const submit = document.getElementById("Submit")
+const title = document.getElementById("Title");
+const author = document.getElementById("Author");
+const status = document.getElementById("Status");
+const submit = document.getElementById("Submit");
 
-const table = document.getElementById("tableDisplay")
+const table = document.getElementById("tableDisplay");
+let myLibrary = [];
 
 
-// submit.addEventListener("click", function(e){
-//     document.createElement("tr")
 
-// })
-let newRow = table.insertRow();
-let newCell = newRow.insertCell();
-newCell.innerText = "The Warehouse";
+function Book(title, author, pages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+}
+
+
+// Used to add row to table
+Book.prototype.addTr = function() {
+    const newRow = table.insertRow();
+    const titleCell = newRow.insertCell();
+    titleCell.innerText = this.title;
+    const authorCell = newRow.insertCell();
+    authorCell.innerText = this.author;
+    const pagesCell = newRow.insertCell();
+    pagesCell.innerText = this.pages;
+
+
+    let self = this
+    const statusCell = newRow.insertCell();
+    const statusBtn = document.createElement("input");
+    statusBtn.type = "button";
+    statusBtn.className = "statusBtn";    
+    if (this.hasRead) {
+        statusBtn.value = "Read";
+    } else {
+        statusBtn.value = "Not Read";
+    }
+    statusBtn.addEventListener("click", function(e) {
+        if (statusBtn.value === "Read") {
+            statusBtn.value = "Not Read";
+            self.hasRead = false;
+        } else {
+            statusBtn.value = "Read";
+            self.hasRead = true;
+        }
+    });
+    statusCell.appendChild(statusBtn);
+
+
+    const removeCell = newRow.insertCell();
+    const removeBtn = document.createElement("input");
+    removeBtn.type = "button";
+    removeBtn.className = "removeBtn";
+    removeBtn.value = "remove";
+    removeBtn.addEventListener("click", function(e) {
+        newRow.remove();
+    });
+    removeCell.appendChild(removeBtn);
+
+
+}
+
+
+
+
+const book1 = new Book("Hello", "Mike", 215, true)
+book1.addTr()
+
+
+
+// let newRow = table.insertRow();
+// let newTitleCell = newRow.insertCell();
+// newTitleCell.innerText = "The Warehouse";
+// let newAuthorCell = newRow.insertCell();
+// newAuthorCell.innerText = "Mike Pike"
+// let newPagesCell = newRow.insertCell();
+// newPagesCell.innerText = "213"
+
+
+// let newStatusCell = newRow.insertCell();
+// var btn = document.createElement('input');
+// btn.type = "button";
+// newStatusCell.appendChild(btn);
+// btn.value = "Read" 
 
